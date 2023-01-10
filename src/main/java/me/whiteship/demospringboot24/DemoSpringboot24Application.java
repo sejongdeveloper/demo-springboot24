f8a82e8a66ebcf1d26c744ef6fe6c53815b5cf66
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-10 23:25:16 +0900

    설정 파일 추가

---
+++ src/main/java/me/whiteship/demospringboot24/DemoSpringboot24Application.java
@@ -0,0 +1,34 @@
+package me.whiteship.demospringboot24;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class DemoSpringboot24Application {
+
+	@Value("${my.message}")
+	private String myMessage;
+
+	@Autowired
+	private Environment environment;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringboot24Application.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner applicationRunner() {
+		return args -> System.out.println(myMessage +
+				" " + Arrays.toString(environment.getActiveProfiles()) +
+				" " + Arrays.toString(environment.getDefaultProfiles()));
+	}
+
+}
